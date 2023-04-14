b439884f6dfa6900bf8dd53ccc975711c70f5573
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-04-13 22:25:20 -0700

    updated views

--- backend/indicators/urls.py
+++ backend/indicators/urls.py
@@ -3,15 +3,15 @@ from . import views
 
 
 urlpatterns = [
-  path("yield", views.get_yield_data, name="yield"),
-  path("ffr", views.get_federal_funds_data, name="fed-funds-rate"),
-  path("csnhpi", views.get_case_shiller_data, name="cash-shiller"),
-  path("unemployment", views.get_unemployment_data, name="unemployment"),
-  path("cpi", views.get_cpi_data, name="cpi"),
-  path("pce", views.get_personal_consumption_data, name="personal-consumption"),
-  path("jolts/openings", views.get_jolts_opening_data, name="jolts-openings"),
-  path("jolts/hires", views.get_jolts_hires_data, name="jolts-hires"),
-  path("jolts/turnover", views.get_jolts_turnover_data, name="jolts-turnover"),
-  path("psr", views.get_personal_savings_data, name="personal-savings"),
-  path("cci", views.get_consumer_confidence_data, name="consumer-confidence"),
+  path("indicator/yield", views.get_yield_data, name="yield"),
+  path("indicator/ffr", views.get_federal_funds_data, name="fed-funds-rate"),
+  path("indicator/csnhpi", views.get_case_shiller_data, name="cash-shiller"),
+  path("indicator/unemployment", views.get_unemployment_data, name="unemployment"),
+  path("indicator/cpi", views.get_cpi_data, name="cpi"),
+  path("indicator/pce", views.get_personal_consumption_data, name="personal-consumption"),
+  path("indicator/jolts/openings", views.get_jolts_opening_data, name="jolts-openings"),
+  path("indicator/jolts/hires", views.get_jolts_hires_data, name="jolts-hires"),
+  path("indicator/jolts/turnover", views.get_jolts_turnover_data, name="jolts-turnover"),
+  path("indicator/psr", views.get_personal_savings_data, name="personal-savings"),
+  path("indicator/cci", views.get_consumer_confidence_data, name="consumer-confidence"),
 ]
--- backend/indicators/views.py
+++ backend/indicators/views.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 FRED_API_KEY = os.getenv('FRED_API_KEY')
+fred_url = 'https://api.stlouisfed.org/fred/series/observations'
 
 # Create your views here.
 def get_yield_data(request):
@@ -17,36 +18,126 @@ def get_yield_data(request):
       'observation_start': '2023-04-01'
     }
 
-    r = requests.get('https://api.stlouisfed.org/fred/series/observations', params=payload)
+    r = requests.get(fred_url, params=payload)
     data = r.json()
     return JsonResponse(data)
 
 def get_federal_funds_data(request):
-    return HttpResponse("fed funds")
+    payload = {
+      'series_id': 'EFFR',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_case_shiller_data(request):
-    return HttpResponse("case shiller")
+    payload = {
+      'series_id': 'CSUSHPINSA',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_unemployment_data(request):
-    return HttpResponse("unemployment")
+    payload = {
+      'series_id': 'UNRATE',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_cpi_data(request):
-    return HttpResponse("cpi")
+    payload = {
+      'series_id': 'CPIAUCSL',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_personal_consumption_data(request):
-    return HttpResponse("personal consumption")
+    payload = {
+      'series_id': 'PCE',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_jolts_opening_data(request):
-    return HttpResponse("jolts opening")
+    payload = {
+      'series_id': 'JTSJOL',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_jolts_hires_data(request):
-    return HttpResponse("jolts hires")
+    payload = {
+      'series_id': 'JTSHIR',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_jolts_turnover_data(request):
-    return HttpResponse("jolts turnovers")
+    payload = {
+      'series_id': 'JTSTSR',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_personal_savings_data(request):
-    return HttpResponse("personal savings")
+    payload = {
+      'series_id': 'PSAVERT',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
 
 def get_consumer_confidence_data(request):
-    return HttpResponse("consumer confidence")
+    payload = {
+      'series_id': 'CSCICP03USM665S',
+      'api_key': FRED_API_KEY,
+      'file_type': 'json',
+      'observation_start': '2023-04-01'
+    }
+
+    r = requests.get(fred_url, params=payload)
+    data = r.json()
+    return JsonResponse(data)
