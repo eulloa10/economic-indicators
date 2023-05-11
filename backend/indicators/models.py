e9dfed6a0b0853dfffc5de197d9e452ec19715c0
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-05-11 01:00:52 -0700

    setting up frontend

--- backend/indicators/models.py
+++ backend/indicators/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Indicators(models.Model):
+  id = models.AutoField(primary_key=True)
+  indicator_name = models.CharField(max_length=None)
+  indicator_value = models.DecimalField(decimal_places=2)
+  indicator_description = models.TextField()
+  indicator_date = models.DateField()
+  created_at = models.DateTimeField()
+  updated_at = models.DateTimeField(auto_now=True)
