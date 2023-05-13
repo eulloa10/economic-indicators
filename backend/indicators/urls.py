1650630937ee7d3441583a58a5503eb5524fbe70
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-05-13 15:16:22 -0700

    updated urls and views

--- backend/indicators/urls.py
+++ backend/indicators/urls.py
@@ -3,15 +3,5 @@ from . import views
 
 
 urlpatterns = [
-  path("indicator/yield", views.get_yield_data, name="yield"),
-  path("indicator/ffr", views.get_federal_funds_data, name="fed-funds-rate"),
-  path("indicator/csnhpi", views.get_case_shiller_data, name="cash-shiller"),
-  path("indicator/unemployment", views.get_unemployment_data, name="unemployment"),
-  path("indicator/cpi", views.get_cpi_data, name="cpi"),
-  path("indicator/pce", views.get_personal_consumption_data, name="personal-consumption"),
-  path("indicator/jolts/openings", views.get_jolts_opening_data, name="jolts-openings"),
-  path("indicator/jolts/hires", views.get_jolts_hires_data, name="jolts-hires"),
-  path("indicator/jolts/turnover", views.get_jolts_turnover_data, name="jolts-turnover"),
-  path("indicator/psr", views.get_personal_savings_data, name="personal-savings"),
-  path("indicator/cci", views.get_consumer_confidence_data, name="consumer-confidence"),
+    path('indicator/<indicator>', views.get_indicator_data, name='indicators')
 ]
--- backend/indicators/views.py
+++ backend/indicators/views.py
@@ -7,137 +7,31 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 FRED_API_KEY = os.getenv('FRED_API_KEY')
-fred_url = 'https://api.stlouisfed.org/fred/series/observations'
+fred_api_url = 'https://api.stlouisfed.org/fred/series/observations'
+
+indicator_series_ids = {
+    'yield_curve': 'DGS10',
+    'fed_funds_rate': 'EFFR',
+    'case_shiller': 'CSUSHPINSA',
+    'unemployment_rate': 'UNRATE',
+    'cpi': 'CPIAUCSL',
+    'personal_consumption': 'PCE',
+    'jolts_opening': 'JTSJOL',
+    'jolts_hires': 'JTSHIR',
+    'jolts_turnover': 'JTSTSR',
+    'personal_savings': 'PSAVERT',
+    'consumer_confidence': 'CSCICP03USM665S'
+}
 
 # Create your views here.
-def get_yield_data(request):
-    payload = {
-      'series_id': 'DGS10',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
+def get_indicator_data(request, indicator):
+	payload = {
+		'series_id': indicator_series_ids[indicator],
+		'api_key': FRED_API_KEY,
+		'file_type': 'json',
+		'observation_start': '2023-04-01'
+	}
 
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_federal_funds_data(request):
-    payload = {
-      'series_id': 'EFFR',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_case_shiller_data(request):
-    payload = {
-      'series_id': 'CSUSHPINSA',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_unemployment_data(request):
-    payload = {
-      'series_id': 'UNRATE',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_cpi_data(request):
-    payload = {
-      'series_id': 'CPIAUCSL',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_personal_consumption_data(request):
-    payload = {
-      'series_id': 'PCE',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_jolts_opening_data(request):
-    payload = {
-      'series_id': 'JTSJOL',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_jolts_hires_data(request):
-    payload = {
-      'series_id': 'JTSHIR',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_jolts_turnover_data(request):
-    payload = {
-      'series_id': 'JTSTSR',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_personal_savings_data(request):
-    payload = {
-      'series_id': 'PSAVERT',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
-
-def get_consumer_confidence_data(request):
-    payload = {
-      'series_id': 'CSCICP03USM665S',
-      'api_key': FRED_API_KEY,
-      'file_type': 'json',
-      'observation_start': '2023-04-01'
-    }
-
-    r = requests.get(fred_url, params=payload)
-    data = r.json()
-    return JsonResponse(data)
+	r = requests.get(fred_api_url, params=payload)
+	data = r.json()
+	return JsonResponse(data)
