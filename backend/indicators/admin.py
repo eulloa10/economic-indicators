5e0c62def129e033fd3f53f553729b44346e3dea
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-05-17 23:37:58 -0700

    updated models

--- backend/indicators/admin.py
+++ backend/indicators/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import IndicatorReference
 
 # Register your models here.
+
+class IndicatorReferenceAdmin(admin.ModelAdmin):
+  list_filter = ("indicator_name", )
+  list_display = ("indicator_name", "indicator_value", "indicator_description", "indicator_date")
+
+admin.site.register(IndicatorReference, IndicatorReferenceAdmin)
--- backend/indicators/models.py
+++ backend/indicators/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Indicators(models.Model):
-  id = models.AutoField(primary_key=True)
-  indicator_name = models.CharField(max_length=None)
-  indicator_value = models.DecimalField(max_digits=10, decimal_places=2)
-  indicator_description = models.TextField()
-  indicator_date = models.DateField()
-  created_at = models.DateTimeField()
-  updated_at = models.DateTimeField(auto_now=True)
+class IndicatorReference(models.Model):
+    indicator_series_id = models.CharField(max_length=None, blank=False)
+    indicator_name = models.CharField(max_length=None)
+    indicator_description = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+      db_table = "indicator_references"
+
+class Indicator(models.Model):
+    # indicator_reference_id as FK
+    indicator_value = models.DecimalField(max_digits=10, decimal_places=2)
+    indicator_date = models.DateField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
