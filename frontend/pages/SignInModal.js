0f4a20e81894e2a0f7017011f43853177f5b7612
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-05-30 16:29:22 -0700

    added signup modal logic

--- frontend/pages/SignInModal.js
+++ frontend/pages/SignInModal.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
 import styles from '../styles/Modals/SignInModal.module.css'
 
 function SignInModal({setShowSignIn}) {
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  function toggleSignUp(e) {
+    e.preventDefault();
+    setShowSignUp(!showSignUp)
+  }
   return (
     <div className={styles['signin-modal-container']}>
       <form className={styles['signin-form']}>
       <h2 className={styles['signin-form-title']}>State of the Market</h2>
-      <p className={styles['signin-form-title']}>Sign in with email</p>
+      <p className={styles['signin-form-title']}>{!showSignUp ? "Sign in with email" : "Create a new account"}</p>
       <div className={styles['signin-data-inputs']}>
         <div className={styles['signin-email-input']}>
           <label className={styles['signin-labels']} for="email">Email</label>
@@ -15,12 +23,21 @@ function SignInModal({setShowSignIn}) {
           <label className={styles['signin-labels']} for="password">Password</label>
           <input className={styles['signin-input-boxes']} type="password" name="password" id="password" required/>
         </div>
+        { showSignUp && <div className={styles['signin-password-input']}>
+          <label className={styles['signin-labels']} for="password">Confirm password</label>
+          <input className={styles['signin-input-boxes']} type="password" name="password" id="password" required/>
+        </div>}
       </div>
       <div className={styles['create-account-link-container']}>
-        <p className={styles['create-account-link']}>Register as new user</p>
+        <button onClick={toggleSignUp} className={styles['create-account-btn']}>
+          {!showSignUp ? "Register as new user?" : "Sign in with existing account?"}
+        </button>
       </div>
-      <button className={styles['signin-submit-btn']} onClick={() => setShowSignIn(false)}>Sign In</button>
-      <button className={styles['signin-submit-btn']} onClick={() => setShowSignIn(false)}>Proceed as Guest</button>
+      {!showSignUp && <button className={styles['signin-submit-btn']} onClick={() => setShowSignIn(false)}>Sign in</button>
+      }
+      {showSignUp && <button className={styles['signin-submit-btn']} onClick={() => setShowSignIn(false)}>Create account</button>
+      }
+      showSignUp && <button className={styles['signin-submit-btn']} onClick={() => setShowSignIn(false)}>Proceed as guest</button>
       </form>
     </div>
   )
