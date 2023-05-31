ef0a12d9dd5152701979337ec0255af22a6446ae
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-05-30 23:17:41 -0700

    updated dashboard page

--- frontend/pages/dashboard/DashboardTable.js
+++ frontend/pages/dashboard/DashboardTable.js
@@ -4,7 +4,28 @@ import styles from '../../styles/Dashboard/DashboardTable.module.css';
 function DashboardTable() {
   return (
     <div className={styles['dashboard-table']}>
-      Dashboard Table
+      <div className={styles['dashboard-small-table']}>
+        <ul className={styles['table-header']}>
+          <li className={styles['table-header-column-names']}>
+            Indicator
+          </li>
+          <li className={styles['table-header-column-names']}>
+            Current Period (CP)
+          </li>
+          <li className={styles['table-header-column-names']}>
+            Prior Period (PP)
+          </li>
+          <li className={styles['table-header-column-names']}>
+            CP Value
+          </li>
+          <li className={styles['table-header-column-names']}>
+            PP Value
+          </li>
+          <li className={styles['table-header-column-names']}>
+            Delta
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
