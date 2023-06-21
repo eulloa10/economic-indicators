c080ccda295752e5cfbf28887e2ed8b247577942
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-06-21 12:43:29 -0700

    added first business day logic

--- backend/reports/views.py
+++ backend/reports/views.py
@@ -6,7 +6,8 @@ import requests
 import os
 import json
 import ast
-from datetime import date, timedelta
+import holidays
+from datetime import datetime, date, timedelta
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -35,16 +36,33 @@ payload = {
     'sort_order': 'desc'
 }
 
+def first_business_day(recent_period):
+    us_holidays = holidays.US()
+    date_format = '%Y-%m-%d'
+    date_obj = datetime.strptime(recent_period, date_format)
+    day_num = date_obj.strftime("%d")
+    first_business_day_of_month = date_obj - timedelta(days=int(day_num) - 1)
+
+    is_holiday = first_business_day_of_month in us_holidays
+    is_weekend = first_business_day_of_month.weekday() > 4
+
+    while is_holiday == True or is_weekend == True:
+      first_business_day_of_month += datetime.timedelta(days = 1)
+
+    return first_business_day_of_month
+
 def extract_recent_and_prior_data(indicator, observations):
     recent_and_prior_observations = {}
-
-    if indicator in daily_indicators:
-      recent_period_date
 
     recent_period_date = observations[0]['date']
     recent_period_data_point = observations[0]['value']
     prior_period_date = observations[1]['date']
     prior_period_data_point = observations[1]['value']
+
+    if indicator in daily_indicators:
+      recent_period_date = first_business_day(recent_period_date).strftime("%m/%d/%Y")
+      print("RPD: ", recent_period_date)
+
     recent_and_prior_observations[recent_period_date] = recent_period_data_point
     recent_and_prior_observations[prior_period_date] = prior_period_data_point
     print(recent_and_prior_observations)
